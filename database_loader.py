b912a519ae026777a7526083bfa97ff3a1eee17f
Ashish Shetty aka AlphaSierra <30448930+Shetty073@users.noreply.github.com>
2020-04-05 17:05:32 +0530

    index_table creation for easy data retrieval

--- database_loader.py
+++ database_loader.py
@@ -1,10 +1,13 @@
 import json, mysql.connector, sys
 
-mydb = mysql.connector.connect(
+try:
+    mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password=""
 )
+except Exception as e:
+    print(e)
 
 mycursor = mydb.cursor()
 
@@ -15,25 +18,37 @@ mycursor.execute("USE bikes")
 
 ALL_BIKES_DATA = {}
 
-def load_json():
-    # Open json_dile and load the data into python dictionary
-    with open("all_bikes_data.json", "r") as json_file:
-        json_data = json_file.read()
-        ALL_BIKES_DATA = json.loads(json_data)
+# Open json_dile and load the data into python dictionary
+with open("all_bikes_data.json", "r") as json_file:
+    json_data = json_file.read()
+    ALL_BIKES_DATA = json.loads(json_data)
 
 
 def delete_all_tables():
-    for brand in ALL_BIKES_DATA.keys():
-        print(f"Dropping tables {brand}")
-        mycursor.execute(F"DROP TABLES IF EXISTS {brand}")
+    mycursor.execute("DROP DATABASE IF EXISTS bikes")
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS bikes")
+    mycursor.execute("USE bikes")
+
+
+def create_index_table():
+    table = "index_table"
+    col_names = "model_name VARCHAR(255), table_name VARCHAR(255)"
+    mycursor.execute(f"CREATE TABLE IF NOT EXISTS {table} ({col_names})")
+    for k, v in ALL_BIKES_DATA.items():
+        for k, v in v.items():
+            model_name = k.lower()
+            sql = f"INSERT INTO {table} (model_name) VALUES(%s)"
+            val = (model_name,)
+            mycursor.execute(sql, val)
+            mydb.commit()
 
 
 def update_database():
-    # Load data from 'all_bikes-data.json' into dictionary
-    load_json()
-
     # Delete all existing tables first
     delete_all_tables()
+
+    # Call create_index_table
+    create_index_table()
 
     # Prepare columns list
     values_list = []
@@ -66,6 +81,11 @@ def update_database():
                 val = (model_name,)
                 mycursor.execute(sql, val)
                 mydb.commit()
+            print(f"\t\tInserting index_table entry for easy retrieval of data")
+            sql = f"UPDATE index_table SET table_name='{table}' WHERE model_name=%s"
+            val = (model_name,)
+            mycursor.execute(sql, val)
+            mydb.commit()
 
 
 update_database()
