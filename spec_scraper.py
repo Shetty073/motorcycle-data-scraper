9a6e048f66eebb1fa008e90e7be6edda9686ecca
Ashish Shetty aka AlphaSierra <30448930+Shetty073@users.noreply.github.com>
2020-04-05 12:13:21 +0530

    dictionary fixed

--- spec_scraper.py
+++ spec_scraper.py
@@ -24,16 +24,16 @@ def parse_spec_page(brand_name, spec_page_url):
     image_div = soup.find("div", class_="imgleft")
     image = image_div.find("a", href=f"{spec_page_url[25:][:-15]}")
     image_tag = image.find("img")
-    ALL_DATA_DICT[brand_name]["bike_pic"] = image_tag["src"] # bike_picture
+    bike_picture = image_tag["src"] # bike_picture
 
     # Get bike name
-    ALL_DATA_DICT[brand_name]["bike_model_name"] = image_tag["alt"] # bike_model_name
+    bike_model_name = image_tag["alt"] # bike_model_name
 
     # Get the price range
     price_div = soup.find("div", class_="price")
     price_inside_div = price_div.find("span")
     price_span = price_inside_div.find("span")
-    ALL_DATA_DICT[brand_name]["bike_price"] = price_span.get_text() # bike_price
+    bike_price = price_span.get_text() # bike_price
 
     # Get technical specs
     specs_dict = {}
@@ -42,10 +42,15 @@ def parse_spec_page(brand_name, spec_page_url):
     for table in spec_tables:
         table_rows = table.find_all("tr")
         for data in table_rows:
-            key = data.find("td").get_text()
+            key = data.find("td").get_text().lower()
+            key = key.replace(" ", "_")
             value = data.find("td", class_="right").get_text()
             specs_dict[key] = value
-    ALL_DATA_DICT[brand_name]["bike_specs"] = specs_dict
+    # Store all info into ALL_DATA_DICT
+    ALL_DATA_DICT[brand_name][bike_model_name] = {}
+    ALL_DATA_DICT[brand_name][bike_model_name]["bike_picture"] = bike_picture
+    ALL_DATA_DICT[brand_name][bike_model_name]["bike_price"] = bike_price
+    ALL_DATA_DICT[brand_name][bike_model_name]["bike_specs"] = specs_dict
 
 
 def parse_spec_links(brand_name, brand_links_list):
@@ -60,12 +65,14 @@ def parse_spec_links(brand_name, brand_links_list):
 
 
 def start():
-    if os.stat("brand_links.json") == 0:
-        print("'brand_links.json' is empty..")
-        print("Starting main scraper..")
-        start_main_scraper()
-    print("Loading data from 'brand_links.json' to a dictionary")
     with open("brand_links.json", "r") as json_file:
+        # Check if file is empty
+        one_char = json_file.read(1)
+        if not one_char:
+            print("'brand_links.json' is empty..")
+            print("Starting main scraper..")
+            start_main_scraper()
+        print("Loading data from 'brand_links.json' to a dictionary")
         all_brand_bike_pages_json = json_file.read()
     all_brand_bike_pages_dict = json.loads(all_brand_bike_pages_json)
     total_brands = len(all_brand_bike_pages_dict)
